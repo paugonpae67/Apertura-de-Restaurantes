b0da50efc84fb4a658fd2a9701e92bb2062eec3b
paula <paulagonzalezllanes@gmail.com>
2024-05-18 18:03:44 +0200

    first commit

---
+++ DeliverUS-Backend/src/middlewares/RestaurantMiddleware.js
@@ -0,0 +1,42 @@
+import { Restaurant, Order } from '../models/models.js'
+
+const checkRestaurantOwnership = async (req, res, next) => {
+  try {
+    const restaurant = await Restaurant.findByPk(req.params.restaurantId)
+    if (req.user.id === restaurant.userId) {
+      return next()
+    }
+    return res.status(403).send('Not enough privileges. This entity does not belong to you')
+  } catch (err) {
+    return res.status(500).send(err)
+  }
+}
+const restaurantHasNoOrders = async (req, res, next) => {
+  try {
+    const numberOfRestaurantOrders = await Order.count({
+      where: { restaurantId: req.params.restaurantId }
+    })
+    if (numberOfRestaurantOrders === 0) {
+      return next()
+    }
+    return res.status(409).send('Some orders belong to this restaurant.')
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
+}
+
+const checkNoOrderNull = async (req, res, next) => {
+  try {
+    const orders = Order.findOne({
+      where: { restaurantId: req.params.restaurantId, deliveredAt: null }
+    })
+    if (orders === null) {
+      return next()
+    }
+    return res.status(401).send('The restaurant has orders with deliveredAt null')
+  } catch (err) {
+    return res.status(500).send(err.message)
+  }
+}
+
+export { checkRestaurantOwnership, restaurantHasNoOrders, checkNoOrderNull }
